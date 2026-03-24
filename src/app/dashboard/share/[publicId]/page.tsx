c0b09cd99f5ead2550d298c26afbb7e887e84941
Dominic <dominicmcdev@gmail.com>
2026-03-23 23:10:15 -0500

    add stuff to not-yet-created pages to prevent next.js from complaining

--- src/app/dashboard/share/[publicId]/page.tsx
+++ src/app/dashboard/share/[publicId]/page.tsx
@@ -1,1 +1,8 @@
 // This page is for our sharable recipe links. All it will do is redirect to the normal (slug) recipe url.
+export default function ShareRecipePage() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <p className="text-2xl font-semibold">Redirecting...</p>
+    </div>
+  );
+}
