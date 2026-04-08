9166a962474d99318984dd3fa8fe058fe9aab4fd
Dominic <dominicmcdev@gmail.com>
2026-04-08 18:11:59 -0500

    change favicon

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,10 @@
 import { DashNavbar } from "@/app/dashboard/_components/DashNavbar";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "ChopChop | Dashboard",
+  description: "The Digital Cookbook",
+};
 
 export default function LandingLayout({
   children,
