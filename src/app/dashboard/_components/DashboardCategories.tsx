d6dabcdc9fbe83d907947d144f1e17a3964fea3c
Dominic <dominicmcdev@gmail.com>
2026-04-19 22:32:52 -0500

    category cards don't resize but instead just scroll into edges

--- src/app/dashboard/_components/DashboardCategories.tsx
+++ src/app/dashboard/_components/DashboardCategories.tsx
@@ -23,16 +23,13 @@ export default async function DashboardCategories() {
 
       {allCategories.length > 0 ? (
         <div className="px-12">
-          <Carousel opts={{ align: "start", loop: true }} className="w-full">
+          <Carousel className="w-full">
             <CarouselContent className="-ml-1">
               {allCategories.map((category) => (
-                <CarouselItem
-                  key={category.id}
-                  className="pl-2 basis-1/2 sm:basis-1/4 md:basis-1/4 lg:basis-1/6 xl:basis-1/8 2xl:basis-1/10"
-                >
+                <CarouselItem key={category.id} className="pl-2 basis-54">
                   <div className="p-1">
                     <Card className="shadow">
-                      <CardContent className="flex h-32 items-center justify-center p-6">
+                      <CardContent className="flex h-28 items-center justify-center p-6">
                         <span className="text-xl font-semibold text-wrap text-center">
                           <Link href={`/dashboard/c/${category.slug}`}>
                             {category.name}
