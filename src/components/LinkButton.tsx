b74b2cd520f7d1260b0b62fd17c009a69e0885e0
Dominic <dominicmcdev@gmail.com>
2026-04-25 16:56:23 -0500

    fix linkbutton to be follow semantic standards, add cursor:pointer to globals.css, remove unecessary use client from button.tsx

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -1,38 +1,31 @@
-"use client";
-
 import React from "react";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import { type VariantProps } from "class-variance-authority";
 import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
-// Extend the props of the base Button to include href
 interface LinkButtonProps
   extends
-    React.ComponentPropsWithoutRef<typeof Button>,
-    VariantProps<typeof buttonVariants> {
-  href: string;
-}
+    React.ComponentPropsWithoutRef<typeof Link>,
+    VariantProps<typeof buttonVariants> {}
 
 export function LinkButton({
-  href,
   children,
   className,
+  variant,
+  size,
   ...props
 }: LinkButtonProps) {
   return (
-    <Button
+    <Link
       {...props}
-      // Merge custom <LinkButton> look with any classes passed from the parent
       className={cn(
+        buttonVariants({ variant, size }),
         "px-6 text-sm font-extrabold uppercase tracking-widest transition-all duration-300 rounded-none",
         className,
       )}
-      nativeButton={false}
-      render={<Link href={href} />}
     >
       {children}
-    </Button>
+    </Link>
   );
 }
